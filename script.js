7f0fed5c84240b67ce2c0311d900de7041e4921c
Kaiky Souza <kaikybelga@gmail.com>
2025-04-13 00:18:25 -0300

    Projeto: Gerenciamento

---
+++ script.js
@@ -0,0 +1,17 @@
+/*function logar(nome, email, cpf, senha) {
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let cpf = document.getElementById("cpf").value; 
+    let senha = document.getElementById("senha").value; 
+
+    if (nome === 'Kaiky' && email === 'kaikybelga@gmail.com' && cpf === '704.203.576-37' && senha === '2005') {
+         
+        alert("Logado com sucesso!");
+    }else{ 
+        alert("Informações incorretas!");
+    }
+
+}
+*/ 
+
+
